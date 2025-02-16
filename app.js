21bec530e564874da09fb9f0905800378a29fa0e
arman8808 <armanal3066@gmail.com>
2025-02-16 20:32:37 +0530

    change in cors url

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import connectDB from './Db/connection.js';
-import pollRoutes from './route/Poll.route.js';
-
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import connectDB from "./Db/connection.js";
+import pollRoutes from "./route/Poll.route.js";
 
 dotenv.config();
 
-
 const app = express();
 
+const allowedOrigins = [
+  "https://fullstactassigment-frontend.vercel.app/",
+  "https://fullstactassigment-fron-git-17ea96-portfolios-projects-771db464.vercel.app/",
+  "https://fullstactassigment-frontend-hgo8m8so1.vercel.app/",
+  "http://localhost:3000",
+];
 
 const corsOptions = {
-    origin: 'https://fullstactassigment-frontend-hgo8m8so1.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-  };
-  
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-  app.use(cors(corsOptions));
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
-
 
 connectDB();
 
-
-
-app.use('/api/v1/polls', pollRoutes);
-app.get('/',async(req,res)=>{
-    res.status(200).json({message:'Server is Live'})
-})
-
+app.use("/api/v1/polls", pollRoutes);
+app.get("/", async (req, res) => {
+  res.status(200).json({ message: "Server is Live" });
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
